ec2020425c27c6d60c955edc3ceac71b161de70a
Alexander <smithalexander05@gmail.com>
2020-02-24 21:17:45 -0500

    working with data file not working

--- project1.sh
+++ project1.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Written by Alexander Smith on 2/24/2020
-quit_game=false
-quit_app=false
-guess=0
-playagain=""
+
+#check if they provided a max number argument
 if [ -z "$1" ]
 then
 	    echo "Please provide a maximum number argument"
@@ -11,14 +9,27 @@ then
 	    exit 0
 fi
 
+quit_game=false
+quit_app=false
+guess=0
+playagain=""
+complete_games=0
+
+file="/home/alexander/Desktop/Programming/UNIX CLASS/project1/HighLow.dat"
+while IFS=: read -r f1 f2 f3 f4 f5 f6 f7
+do
+        # display fields using f1, f2,..,f7
+        printf 'Guesses: %s, Games Played: %s, Home Dir: %s\n' "$f1" "$f7" "$f6"
+done <"$file"
+
 while [ $quit_app = false ]
 do
 	rand=$((($RANDOM%$1)+1))
 	amount=0
 	while [ $quit_game = false ]
 	do
-		echo input your guess: 
-		read guess
+		printf "input your guess: " 
+		read -r guess
 		if [ $guess -gt $1 ]
 		then
 			echo please input a value between 1 and $1
@@ -43,17 +54,24 @@ do
 			fi
 		fi
 
-		
+	complete_games=$((complete_games+1))	
 	done
-	echo "Would you like to play again (Y/N): "
+	printf "Would you like to play again (Y/N): "
 	read -r playagain
-	if [ $playagain == "Y" ] || [ $playagain == "y" ]
+	if [ $playagain == "Y" ] || [ $playagain == "y" ] || [ $playagain == "" ]
 	then
 		quit_game=false
 		clear
 	else
-		echo Thanks for Playing!
-		quit_app=true
+		if [ $playagain == "N" ] || [ $playagain == "n" ]
+		then
+			echo Thanks for Playing!
+			quit_app=true
+		else 
+			echo Please enter Y or N.
+		fi
 	fi
 done
+echo $amount >> HighLow.dat
+echo $complete_games >>HighLow.dat
 exit 0
